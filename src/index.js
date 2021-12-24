b1ac9bd6526481f1aed4774a26b7a020ce596903
Nataliia Lyfar <lifarnatala@gmail.com>
2021-12-24 21:27:37 +0200

    started slick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './sass/main.scss';
 import '../js/mobile-menu';
 import '../js/modal';
+import '../js/animate';
+import 'jquery';
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
